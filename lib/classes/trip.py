0124e65b8fb3a2c6469ab5d098ca00b0b9028804
Michael Jeung <michaelj229@gmail.com>
2023-05-08 13:51:40 -0400

    done

--- lib/classes/trip.py
+++ lib/classes/trip.py
@@ -6,3 +6,9 @@ class Trip:
         self.national_park = national_park
         self.start_date = start_date
         self.end_date = end_date
+
+        visitor.trips(self)
+        visitor.national_parks(national_park)
+
+        national_park.trips(self)
+        national_park.visitors(visitor)
